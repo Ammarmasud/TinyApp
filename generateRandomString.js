8232adc7cbb142eccdde2878dfaf058e168f1974
Ammar <ammarmasud@gmail.com>
2017-02-16 15:05:44 +0000

    added user specific features. Users only have access to their own custom list of short urls

--- generateRandomString.js
+++ generateRandomString.js
@@ -1,11 +1,16 @@
-function generateRandomString() {
+function generateRandomString(urls) {
   const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const shortUrlArr = [];
 
   for (let i = 0; i < 6; i++) {
     shortUrlArr.push(possible.charAt(Math.floor(Math.random() * possible.length)));
   }
-  return shortUrlArr.join("")
+
+  if (urls[shortUrlArr.join("")]) {
+    return generateRandomString(urls);
+  } else {
+    return shortUrlArr.join("");
+  }
 }
 
 module.exports = generateRandomString;
--- routes.js
+++ routes.js
@@ -1,10 +1,15 @@
 const generateRandomString = require('./generateRandomString');
 const checkEmails = require('./checkEmails');
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-const users = {'3Bv2hG':{id:'3Bv2hG',email:'test@test.com',password:'test'}};
+                    "b2xVn2": "http://www.lighthouselabs.ca",
+                    "9sm5xK": "http://www.google.com"
+                    };
+const users = {'3Bv2hG':{
+                  id:'3Bv2hG',
+                  email:'test@test.com',
+                  password:'test',
+                  shortURLs: ["b2xVn2","9sm5xK"]
+              }};
 
 module.exports = (app) => {
   app.get("/", (req, res) => {
@@ -18,10 +23,11 @@ module.exports = (app) => {
 
   app.post("/registration", (req, res) => {
     if (req.body.email && req.body.password && !(checkEmails(users, req.body.email))) {
-      const id = generateRandomString();
+      const id = generateRandomString(users);
       users[id] = {'id':id,
                   'email': req.body.email,
-                  'password': req.body.password};
+                  'password': req.body.password,
+                  'shortURLs': []};
       res.cookie('user_id',id);
       res.redirect('/');
     } else {
@@ -32,7 +38,7 @@ module.exports = (app) => {
 
   // LOGIN
   app.post("/login", (req, res) => {
-    let id = checkEmails(users, req.body.email)
+    let id = checkEmails(users, req.body.email);
     if (id && users[id].password === req.body.password) {
       res.cookie('user_id',id);
       res.redirect('/');
@@ -40,7 +46,6 @@ module.exports = (app) => {
       res.statusCode = 403;
       res.redirect('/login');
     }
-
   });
 
   app.get("/login", (req, res) => {
@@ -48,48 +53,82 @@ module.exports = (app) => {
   });
 
   app.post("/logout", (req, res) => {
-    let id = req.cookies.user_id
-    res.clearCookie('user_id',id);
+    res.clearCookie('user_id',req.cookies.user_id);
     res.redirect('/');
   });
 
   app.get("/urls/new", (req, res) => {
     let email;
-    if (req.cookies.user_id) {
+    if (users[req.cookies.user_id]) {
       email = users[req.cookies.user_id].email;
+      res.render("urls_new",{'email': email});
+    } else {
+      res.statusCode = 403;
+      res.redirect('/login');
     }
-    res.render("urls_new",email);
+
   });
 
   app.post("/urls/:shortURL/delete", (req, res) => {
-    delete urlDatabase[req.params.shortURL];
-    res.redirect('/urls');
+    let index = users[req.cookies.user_id].shortURLs.indexOf(req.params.shortURL)
+    // console.log(index, users[req.cookies.user_id].shortURLs.indexOf(req.params.shortURL));
+    if (index !== -1) {
+      delete urlDatabase[req.params.shortURL];
+      users[req.cookies.user_id].shortURLs.splice(index, 1)
+      res.redirect('/urls');
+    } else if (users[req.cookies.user_id]) {
+      res.statusCode = 403;
+      res.redirect('/urls');
+    } else {
+      res.statusCode = 403;
+      res.redirect('/login');
+    }
+
   });
 
   app.post("/urls/:shortURL", (req, res) => {
-    urlDatabase[req.params.shortURL] = req.body['longURL'];
-    res.redirect('/urls');
+    let index = users[req.cookies.user_id].shortURLs.indexOf(req.params.shortURL)
+    if (index !== -1) {
+      urlDatabase[req.params.shortURL] = req.body['longURL'];
+      users[req.cookies.user_id].push
+      res.redirect('/urls');
+    } else if (users[req.cookies.user_id]) {
+      res.statusCode = 403;
+      res.redirect('/urls');
+    } else {
+      res.statusCode = 403;
+      res.redirect('/login');
+    }
   });
 
   app.get("/urls/:shortURL", (req, res) => {
     const templateVars = {'shortURL': req.params.shortURL,
-                          'longURL': urlDatabase[req.params.shortURL],
-                          'email': users[req.cookies.user_id].email};
+                          'longURL': urlDatabase[req.params.shortURL]};
+    if (users[req.cookies.user_id]) {
+      templateVars['email'] = users[req.cookies.user_id].email;
+    }
     res.render('urls_show', templateVars);
   });
 
   app.post("/urls", (req, res) => {
-    let shortURL = generateRandomString();
-    urlDatabase[shortURL] = req.body['longURL'];
-    res.redirect('/urls');
+    if (users[req.cookies.user_id]) {
+      let shortURL = generateRandomString(urlDatabase);
+      urlDatabase[shortURL] = req.body['longURL'];
+      users[req.cookies.user_id].shortURLs.push(shortURL);
+      res.redirect('/urls');
+    }
   });
 
   app.get("/urls", (req, res) => {
     let email;
-    if (req.cookies.user_id) {
+    let userURLs = [];
+    if (users[req.cookies.user_id]) {
       email = users[req.cookies.user_id].email;
+      userURLs = users[req.cookies.user_id].shortURLs;
     }
+
     const templateVars =  {'urls': urlDatabase,
+                          'userURLs': userURLs,
                           'email': email};
     res.render('urls_index', templateVars);
   });
@@ -104,9 +143,9 @@ module.exports = (app) => {
     }
   });
 
-  app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
+  // app.get("/urls.json", (req, res) => {
+  //   res.json(urlDatabase);
+  // });
 
   app.get("/hello", (req, res) => {
     res.end("<html><body>Hello <b>World</b></body></html>\n"); //can also use `res.send()`
